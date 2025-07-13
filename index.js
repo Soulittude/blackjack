35c2ed15d8be8e1ca10441fa6ecf9f0cc7ddda8f
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-14 00:12:49 +0300

    Added casino option with full functionality

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 let cardsEl = document.getElementById("cards-el");
+let casinoCardsEl = document.getElementById("casino-el");
 let sumEl = document.getElementById("sum-el");
+
 let startBtn = document.getElementById("btn-start");
 let cardBtn = document.getElementById("btn-card");
-let msg = document.getElementById("message");
+let stayBtn = document.getElementById("btn-stay");
+
+let msg = document.getElementById("message-el");
 
 
-let cards = [];
-let sum = 0;
+
+let myCards = [];
+let casinoCards = [];
+let mySum = 0;
+let casinoSum = 0;
 let isAlive = false;
 
 function startGame() {
     resetGame();
-    randomCard();
-    randomCard();
+
+
+    gameUpdate("casino");
+    gameUpdate("player");
+    gameUpdate("player");
 }
 
 function resetGame() {
-    cards = [];
-    sum = 0;
+    myCards = [];
+    casinoCards = [];
+    mySum = 0;
+    casinoSum = 0;
     isAlive = true;
+
     cardsEl.innerText = "";
     sumEl.innerText = "";
+    casinoCardsEl.innerText = "";
     msg.innerText = "";
 }
 
@@ -32,32 +46,65 @@ function randomCard() {
     else if (pickCard === 1) {
         pickCard = 11;
     }
-    gameUpdate(pickCard)
     return pickCard
 }
 
-function gameUpdate(card) {
-    cards.push(card);
-    sum += card;
-    cardsEl.innerText = "Your Cards: " + cards;
-    sumEl.innerText = "Sum: " + sum;
-    gameCheck();
-}
-
-function gameCheck() {
-    if (cards.length < 2) {
+function gameUpdate(side) {
+    card = randomCard();
+    if (side === "player") {
+        myCards.push(card);
+        mySum += card;
+        cardsEl.innerText = "Your Cards: " + myCards;
+        sumEl.innerText = "Sum: " + mySum;
+        gameCheck(side);
         return
     }
-    if (sum < 21) {
-        msg.innerText = ("Press new card to get another card");
+    else if (side === "casino") {
+        casinoCards.push(card);
+        casinoSum += card;
+        casinoCardsEl.innerText = "Casino cards: " + casinoCards;
+        gameCheck(side);
+        return
     }
-    else if (sum === 21) {
-        msg.innerText = ("You got a blackjack! You win!");
-        isAlive = false;
+}
+
+function gameCheck(choose) {
+    if (myCards.length < 2) {
+        return
     }
-    else {
-        msg.innerText = ("You went over 21! You lose!");
-        isAlive = false;
+    
+    if (choose === "player") {
+        if (mySum === 21) {
+            msg.innerText = ("You got a blackjack! You win!");
+            isAlive = false;
+
+        }
+        else if (mySum > 21) {
+            msg.innerText = ("You went over 21! You lose!");
+            isAlive = false;
+        }
+        else {
+            msg.innerText = ("Do you want to new card or stay?");
+        }
+    }
+    else if (choose === "casino" && casinoSum > 20) {
+        if (casinoSum === 21) {
+            msg.innerText = ("Casino blackjack! You lost!");
+            isAlive = false;
+        }
+        else {
+            myScore = 21 - mySum;
+            casinoScore = casinoSum - 21;
+
+            if (casinoScore < myScore) {
+                msg.innerText = ("Casino won... You lost!");
+                isAlive = false
+            }
+            else {
+                msg.innerText = ("Casino lost... You won");
+                isAlive = false
+            }
+        }
     }
     aliveCheck()
     return
@@ -67,10 +114,18 @@ function aliveCheck() {
     if (isAlive === false) {
         startBtn.classList.remove("hide");
         cardBtn.classList.add("hide");
+        stayBtn.classList.add("hide");
     }
     else {
         startBtn.classList.add("hide");
         cardBtn.classList.remove("hide");
-
+        stayBtn.classList.remove("hide");
     }
+}
+
+function stay() {
+    while (casinoSum < 21) {
+        gameUpdate("casino");
+    }
+
 }
